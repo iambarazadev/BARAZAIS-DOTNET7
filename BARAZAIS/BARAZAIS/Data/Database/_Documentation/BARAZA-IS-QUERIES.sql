d06cec9ad91213f8ff9cae82255a9c269d06200a
razackreds <127778424+razackreds@users.noreply.github.com>
2023-04-27 14:01:08 +0300

    Update BARAZA-IS-QUERIES.sql

--- BARAZAIS/BARAZAIS/Data/Database/_Documentation/BARAZA-IS-QUERIES.sql
+++ BARAZAIS/BARAZAIS/Data/Database/_Documentation/BARAZA-IS-QUERIES.sql
@@ -1,7 +1,13 @@
 show databases;
+drop database barazadb;
 create database barazadb;
 use barazadb;
 
 show tables;
 select * from aspnetusers;
 select * from aspnetroles;
+select * from Companies;
+
+insert into Companies values(
+	default,now(),now(),"CMP", "DropCode", "","111-222-333", "info@dropcode.co.tz","0754999954","Ilala, Dar es salaam"
+);
