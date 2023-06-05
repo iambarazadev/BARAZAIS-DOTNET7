28e83605948930527ce2de4657f6cfa05d587364
razackreds <127778424+razackreds@users.noreply.github.com>
2023-06-05 17:12:01 +0300

    environment variable to production

--- Program.cs
+++ Program.cs
@@ -35,7 +35,8 @@ if (app.Environment.IsDevelopment())
 }
 else
 {
-    app.UseExceptionHandler("/Error");
+    //app.UseExceptionHandler("/Error");
+    app.Environment.IsProduction();
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
